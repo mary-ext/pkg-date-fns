d139bad30454a42d8f0d26fdbaaeda48904337c1
Mary <git@mary.my.id>
2025-01-23 15:22:49 +0700

    feat: mark everything as side effect free

--- lib/mod.ts
+++ lib/mod.ts
@@ -8,6 +8,7 @@ export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
  * @param date the date to convert.
  * @returns the Unix timestamp in milliseconds.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const toUnixMilliseconds = (date: Date): number => {
 	return date.getTime();
 };
@@ -17,6 +18,7 @@ export const toUnixMilliseconds = (date: Date): number => {
  * @param date the date to convert.
  * @returns the Unix timestamp in seconds.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const toUnixSeconds = (date: Date): number => {
 	return Math.floor(toUnixMilliseconds(date) / 1_000);
 };
@@ -26,6 +28,7 @@ export const toUnixSeconds = (date: Date): number => {
  * @param date the date to format
  * @returns the formatted date string
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const toISODateString = (date: Date): string => {
 	return date.toLocaleDateString('sv-SE');
 };
@@ -35,6 +38,7 @@ export const toISODateString = (date: Date): string => {
  * @param date the date to format
  * @returns the formatted datetime string
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const toISODateTimeString = (date: Date): string => {
 	return date.toLocaleString('sv-SE').replace(' ', 'T') + '.' + ('' + getMilliseconds(date)).padStart(3, '0');
 };
@@ -44,6 +48,7 @@ export const toISODateTimeString = (date: Date): string => {
  * @param date the date to extract from.
  * @returns the milliseconds portion of the date.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getMilliseconds = (date: Date): number => {
 	return date.getMilliseconds();
 };
@@ -53,6 +58,7 @@ export const getMilliseconds = (date: Date): number => {
  * @param date the date to extract from.
  * @returns the seconds portion of the date.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getSeconds = (date: Date): number => {
 	return date.getSeconds();
 };
@@ -62,6 +68,7 @@ export const getSeconds = (date: Date): number => {
  * @param date the date to extract from.
  * @returns the minutes portion of the date.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getMinutes = (date: Date): number => {
 	return date.getMinutes();
 };
@@ -71,6 +78,7 @@ export const getMinutes = (date: Date): number => {
  * @param date the date to extract from.
  * @returns the hours portion of the date.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getHours = (date: Date): number => {
 	return date.getHours();
 };
@@ -80,6 +88,7 @@ export const getHours = (date: Date): number => {
  * @param date the date to extract from.
  * @returns the day of the week as a number.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getDayOfWeek = (date: Date): DayOfWeek => {
 	return date.getDay() as DayOfWeek;
 };
@@ -89,6 +98,7 @@ export const getDayOfWeek = (date: Date): DayOfWeek => {
  * @param date the date to extract from.
  * @returns the day of the month as a number.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getDayOfMonth = (date: Date): number => {
 	return date.getDate();
 };
@@ -98,6 +108,7 @@ export const getDayOfMonth = (date: Date): number => {
  * @param date the date to extract from.
  * @returns the month as a number.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getMonth = (date: Date): number => {
 	return date.getMonth();
 };
@@ -107,6 +118,7 @@ export const getMonth = (date: Date): number => {
  * @param date the date to extract from.
  * @returns the year as a number.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const getYear = (date: Date): number => {
 	return date.getFullYear();
 };
@@ -117,6 +129,7 @@ export const getYear = (date: Date): number => {
  * @param compare the date to compare against.
  * @returns true if first date is before the second, false otherwise.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const isBeforeDate = (date: Date, compare: Date): boolean => {
 	return date !== compare && toUnixMilliseconds(date) < toUnixMilliseconds(compare);
 };
@@ -127,6 +140,7 @@ export const isBeforeDate = (date: Date, compare: Date): boolean => {
  * @param compare the date to compare against.
  * @returns true if the first date is after the second, false otherwise.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const isAfterDate = (date: Date, compare: Date): boolean => {
 	return date !== compare && toUnixMilliseconds(date) > toUnixMilliseconds(compare);
 };
@@ -137,6 +151,7 @@ export const isAfterDate = (date: Date, compare: Date): boolean => {
  * @param b the second date to compare.
  * @returns true if the two dates are the same, false otherwise.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const isSameDate = (a: Date, b: Date): boolean => {
 	return a === b || toUnixMilliseconds(a) === toUnixMilliseconds(b);
 };
@@ -147,6 +162,7 @@ export const isSameDate = (a: Date, b: Date): boolean => {
  * @param b the second date to compare.
  * @returns true if the two dates fall on the same calendar day, false otherwise.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const isSameCalendarDate = (a: Date, b: Date): boolean => {
 	if (a === b) {
 		return true;
@@ -165,6 +181,7 @@ export const isSameCalendarDate = (a: Date, b: Date): boolean => {
  * @param b the second date to compare.
  * @returns true if the two dates are in the same month, false otherwise.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const isSameCalendarMonth = (a: Date, b: Date): boolean => {
 	if (a === b) {
 		return true;
@@ -182,6 +199,7 @@ export const isSameCalendarMonth = (a: Date, b: Date): boolean => {
  * @param b the second date to compare.
  * @returns true if the two dates are in the same year, false otherwise.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const isSameCalendarYear = (a: Date, b: Date): boolean => {
 	if (a === b) {
 		return true;
@@ -196,6 +214,7 @@ export const isSameCalendarYear = (a: Date, b: Date): boolean => {
  * @param b the second date to compare.
  * @returns the earlier of the two dates.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const min = (a: Date, b: Date): Date => {
 	return isBeforeDate(a, b) ? a : b;
 };
@@ -206,6 +225,7 @@ export const min = (a: Date, b: Date): Date => {
  * @param b the second date to compare.
  * @returns the later of the two dates.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const max = (a: Date, b: Date): Date => {
 	return isAfterDate(a, b) ? a : b;
 };
@@ -217,6 +237,7 @@ export const max = (a: Date, b: Date): Date => {
  * @param max the maximum allowable date.
  * @returns the clamped date.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const clamp = (
 	date: Date,
 	min: Date | undefined,
@@ -238,6 +259,7 @@ export const clamp = (
  * @param date the date to clone.
  * @returns a new date object with the same value as the input date.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const cloneDate = (date: Date): Date => {
 	return new Date(date);
 };
@@ -247,6 +269,7 @@ export const cloneDate = (date: Date): Date => {
  * @param date the date to find the start of the day for.
  * @returns a new date set to the start of the day.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const startOfDay = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -259,6 +282,7 @@ export const startOfDay = (date: Date): Date => {
  * @param date the date to find the end of the day for.
  * @returns a new date set to the end of the day.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const endOfDay = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -271,6 +295,7 @@ export const endOfDay = (date: Date): Date => {
  * @param date the date to find the start of the week for.
  * @returns a new date set to the start of the week.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const startOfWeek = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -284,6 +309,7 @@ export const startOfWeek = (date: Date): Date => {
  * @param date the date to find the end of the week for.
  * @returns a new date set to the end of the week.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const endOfWeek = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -297,6 +323,7 @@ export const endOfWeek = (date: Date): Date => {
  * @param date the date to find the start of the month for.
  * @returns a new date set to the start of the month.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const startOfMonth = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -310,6 +337,7 @@ export const startOfMonth = (date: Date): Date => {
  * @param date the date to find the end of the month for.
  * @returns a new date set to the end of the month.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const endOfMonth = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -323,6 +351,7 @@ export const endOfMonth = (date: Date): Date => {
  * @param date the date to find the start of the year for.
  * @returns a new date set to the start of the year.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const startOfYear = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -336,6 +365,7 @@ export const startOfYear = (date: Date): Date => {
  * @param date the date to find the end of the year for.
  * @returns a new date set to the end of the year.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const endOfYear = (date: Date): Date => {
 	const d = cloneDate(date);
 
@@ -350,6 +380,7 @@ export const endOfYear = (date: Date): Date => {
  * @param milliseconds the number of milliseconds to add.
  * @returns a new date with the added milliseconds.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const addMilliseconds = (date: Date, milliseconds: number): Date => {
 	const d = cloneDate(date);
 
@@ -363,6 +394,7 @@ export const addMilliseconds = (date: Date, milliseconds: number): Date => {
  * @param seconds the number of seconds to add.
  * @returns a new date with the added seconds.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const addSeconds = (date: Date, seconds: number): Date => {
 	const d = cloneDate(date);
 
@@ -376,6 +408,7 @@ export const addSeconds = (date: Date, seconds: number): Date => {
  * @param minutes the number of minutes to add.
  * @returns a new date with the added minutes.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const addMinutes = (date: Date, minutes: number): Date => {
 	const d = cloneDate(date);
 
@@ -389,6 +422,7 @@ export const addMinutes = (date: Date, minutes: number): Date => {
  * @param hours the number of hours to add.
  * @returns a new date with the added hours.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const addHours = (date: Date, hours: number): Date => {
 	const d = cloneDate(date);
 
@@ -402,6 +436,7 @@ export const addHours = (date: Date, hours: number): Date => {
  * @param days the number of days to add.
  * @returns a new date with the added days.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const addDays = (date: Date, days: number): Date => {
 	const d = cloneDate(date);
 
@@ -415,6 +450,7 @@ export const addDays = (date: Date, days: number): Date => {
  * @param months the number of months to add.
  * @returns a new date with the added months.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const addMonths = (date: Date, months: number): Date => {
 	const d = cloneDate(date);
 
@@ -428,6 +464,7 @@ export const addMonths = (date: Date, months: number): Date => {
  * @param years the number of years to add.
  * @returns a new date with the added years.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const addYears = (date: Date, years: number): Date => {
 	const d = cloneDate(date);
 
@@ -441,6 +478,7 @@ export const addYears = (date: Date, years: number): Date => {
  * @param day the target day of the week.
  * @returns a new date set to the previous occurrence of the specified day.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const previousDay = (date: Date, day: DayOfWeek): Date => {
 	let delta = day - getDayOfWeek(date);
 	if (delta >= 0) {
@@ -456,6 +494,7 @@ export const previousDay = (date: Date, day: DayOfWeek): Date => {
  * @param day the target day of the week.
  * @returns a new date set to the next occurrence of the specified day.
  */
+/*#__NO_SIDE_EFFECTS__*/
 export const nextDay = (date: Date, day: DayOfWeek): Date => {
 	let delta = day - getDayOfWeek(date);
 	if (delta <= 0) {
